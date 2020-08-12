f39ba3621999cd54490b2c80b3aa9f502f6b59a7
Alena Nikulina <alenanikulina0@gmail.com>
2020-08-12 17:54:50 -0400

    adding the form component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Button from 'react-bootstrap/Button';
 
-import { Button, Alert, Breadcrumb, BreadcrumbItem, Card } from 'react-bootstrap'
+import { Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form } from 'react-bootstrap'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Form>
+          <Form.Group>
+            <Form.Label>Email Adress</Form.Label>
+            <Form.Control type="email" placeholder="example@gmail.com" />
+          </Form.Group>
+        </Form>
+
         <Card className="mb-3" style={{ color: "#000" }}>
           <Card.Img src="https://picsum.photos/id/237/200/200" />
           <Card.Body>
